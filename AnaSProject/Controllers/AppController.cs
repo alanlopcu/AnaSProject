edcbebceabf1c90ba813e088bd1353ebc04dd45d
alanlopcu <alanlopcu@gmail.com>
2017-11-21 13:12:01 -0500

    Changing others prop

---
+++ AnaSProject/Controllers/AppController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AnaSProject.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}

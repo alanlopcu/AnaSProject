607433a67b85584c43d513aa621463d8789ac14f
alanlopcu <alanlopcu@gmail.com>
2017-11-22 13:58:16 -0500

    Include Entity Framework to this assessment & create some simple Data Entities

---
+++ AnaSProject/Data/AnaSContext.cs
@@ -0,0 +1,17 @@
+﻿using AnaSProject.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaSProject.Data
+{
+    public class AnaSContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
---
+++ AnaSProject/Data/Entities/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnaSProject.Data.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public Category CategoryId { get; set; }
+        //public string Category { get; set; }
+        //public string Size { get; set; }
+        public decimal Price { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+        //public string Dating { get; set; }
+        //public string ArtId { get; set; }
+        //public string Artist { get; set; }
+        //public DateTime ArtistBirthDate { get; set; }
+        //public DateTime ArtistDeathDate { get; set; }
+        //public string ArtistNationality { get; set; }
+    }
+}
